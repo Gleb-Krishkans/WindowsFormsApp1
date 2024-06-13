1596157ebb6a2a7143315a73f3edcac1cab347bb
krish <krish@DESKTOP-6TJNE2J>
2024-06-13 22:27:58 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            Form2 form2 = new Form2(text);
+            form2.Show();
+        }
+    }
+}
